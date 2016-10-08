0835035e2507ffe347aa96eae77cab9404582e86
Pranay Shirolkar <pranayshirolkar@gmail.com>
2016-10-08 04:39:57 -0500

    updating with global scope

--- myAngularApp.js
+++ myAngularApp.js
@@ -9,9 +9,10 @@ app.component('videoList', {
 		'<h3>{{video.name}}</h3>'+
 		'<p><video src="{{video.name}}">Browser Support Error!</video></p></div>',
 	controller: function VideoListController($http){
+		var _this=this;
 		$http.get('videoList.json')
     .success(function(data) {
-		this.videos=data;
+		_this.videos=data;
     })
     .error(function(data,status,error,config){
         this.videos = [{heading:"Error",description:"Could not load json   data"}];
