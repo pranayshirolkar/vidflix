bc70402acce26459c9117e95af38a23e610918a2
Pranay Shirolkar <pranayshirolkar@gmail.com>
2016-10-08 04:32:56 -0500

    Initial Commit

---
+++ myAngularApp.js
@@ -0,0 +1,23 @@
+var app = angular.module("vidFlixApp",[]);
+app.controller("MainCtrl",function($scope){
+
+});
+app.component('videoList', {
+	//Note: URL is relative to the index.html file
+	template:
+		'<div ng-repeat="video in $ctrl.videos" class="well">'+
+		'<h3>{{video.name}}</h3>'+
+		'<p><video src="{{video.name}}">Browser Support Error!</video></p></div>',
+	controller: function VideoListController($http){
+		$http.get('videoList.json')
+    .success(function(data) {
+		this.videos=data;
+    })
+    .error(function(data,status,error,config){
+        this.videos = [{heading:"Error",description:"Could not load json   data"}];
+    });
+	}
+});
+// app.controller("VideoListCtrl",function($scope){
+
+// });
